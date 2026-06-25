67ceb767a4fde01b93fa11549a2004baddca2051
amin ojaghi <kermonshaw@gmail.com>
2026-06-25 10:42:06 +0000

    Implemented RAG + Vector Search using PGVector

--- src/rag_helper.py
+++ src/rag_helper.py
@@ -1,3 +1,6 @@
+from .utils import vec_to_str
+
+
 class RAGBase:
     def __init__(
         self,
@@ -90,3 +93,33 @@ class RAGVector(RAGBase):
             num_results=num_results,
             filter_dict=self.search_filter_dict
         )
+
+
+
+
+class RAGPgVector(RAGBase):
+    def __init__(self, embedder, conn, course = "llm-zoomcamp", **kwargs):
+        super().__init__(index=None, **kwargs)
+        self.embedder = embedder
+        self.conn = conn
+        self.course = course
+
+    def search(self, query, num_results=5):
+        query_vector = self.embedder.encode(query)
+        query_str = vec_to_str(query_vector)
+
+        rows = self.conn.execute(
+            """
+            SELECT course, section, question, answer
+            FROM documents
+            WHERE course = %s
+            ORDER BY embedding <=> %s::vector
+            LIMIT %s
+            """,
+            (self.course, query_str, num_results)
+        ).fetchall()
+
+        return [
+            {"course": r[0], "section": r[1], "question": r[2], "answer": r[3]}
+            for r in rows
+        ]
---
+++ src/utils.py
@@ -0,0 +1,5 @@
+
+
+
+def vec_to_str(vector):
+    return "[" + ",".join(str(x) for x in vector) + "]"
